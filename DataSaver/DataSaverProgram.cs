af5be299b8ad90693e9b4fe3032c08640e2cbe10
Zachary Johnson <johnsonz17@outlook.com>
2019-10-08 21:44:42 -0400

    Add project files.

---
+++ DataSaver/DataSaverProgram.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using Ionic.Zip;
+
+namespace DataSaver
+{
+
+    class DataSaverProgram
+    {
+        string m_monitoredLocation
+        {
+            get; set;
+        }
+        double m_compressionRate
+        {
+            get; set;
+        }
+        int m_compressionTime
+        {
+            get; set;
+        }
+
+        struct myDirectory
+        {
+        }
+
+        public void StartCompression(string monitoredLocation, double compressionRate, int compressionTime)
+        {
+            m_monitoredLocation = monitoredLocation;
+            m_compressionRate = compressionRate;
+            m_compressionTime = compressionTime;
+            DateTime currentTime = DateTime.Now;
+            ScanDirectory(m_monitoredLocation, currentTime);
+        }
+
+        public void ScanDirectory(string rootPath, DateTime currentTime)
+        {
+            string[] folderList = Directory.GetDirectories(rootPath);
+            string[] fileList = Directory.GetFiles(rootPath);
+
+            foreach(string folderPath in folderList)
+            {
+                ScanDirectory(folderPath, currentTime);
+            }
+
+            foreach(string filePath in fileList)
+            {
+                DateTime lastAccessTime = File.GetLastAccessTime(filePath);
+                TimeSpan difference = currentTime - lastAccessTime;
+                if(m_compressionTime < Convert.ToInt32(difference.TotalSeconds))
+                {
+                    CompressFile(filePath);
+                }
+            }
+        }
+
+        public void CompressFile(string filePath)
+        {
+            using (Ionic.Zip.ZipFile zip = new Ionic.Zip.ZipFile())
+            {
+                zip.AddFile(filePath);
+                zip.Save(filePath + ".zip");
+            }
+            File.WriteAllText(filePath, string.Empty);
+        }
+    }
+}
---
+++ DataSaver/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace DataSaver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void File_Click1(object sender, RoutedEventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    UrnTextBlock.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            bool startCompressor = true;
+
+            if(TimeDays.Text == "" && TimeHours.Text == "" && TimeMinutes.Text == "")
+            {
+                System.Windows.Forms.MessageBox.Show("Please enter a valid time", "Menu Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                startCompressor = false;
+            }
+            if(UrnTextBlock.Text == "" || !Directory.Exists(UrnTextBlock.Text))
+            {
+                System.Windows.Forms.MessageBox.Show("Please enter a valid location", "Menu Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                startCompressor = false;
+            }
+
+            if(startCompressor)
+            {
+                string monitoredLocation = UrnTextBlock.Text;
+                double compressionRate = Selector.Value;
+
+                int totalTimeSeconds = 0;
+                int days = 0;
+                int hours = 0;
+                int minutes = 0;
+
+                if(TimeDays.Text != "")
+                {
+                    days = Convert.ToInt32(TimeDays.Text);
+                }
+                if(TimeHours.Text != "")
+                {
+                    hours = Convert.ToInt32(TimeHours.Text);
+
+                }
+                if(TimeMinutes.Text != "")
+                {
+                    minutes = Convert.ToInt32(TimeMinutes.Text);
+                }
+
+                totalTimeSeconds += days * 86400;
+                totalTimeSeconds += hours * 3600;
+                totalTimeSeconds += minutes * 60;
+
+                DataSaverProgram program = new DataSaverProgram();
+                program.StartCompression(monitoredLocation, compressionRate, totalTimeSeconds);
+            }
+        }
+    }
+}
